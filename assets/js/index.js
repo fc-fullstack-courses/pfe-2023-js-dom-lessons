f53c0a1b9320d2e618f388ccad64e28b30832c74
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-22 18:06:40 +0200

    Add clicker task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,20 +22,43 @@ console.dir(document);
 */
 
 // 1 знайти кнопку
-const buttons = document.getElementsByTagName('button');
-const button = buttons[0];
+// const buttons = document.getElementsByTagName('button');
+// const button = buttons[0];
 
 // 2. сказати що при натисканні на кнопку щось має відбутися
 // eventTarget.addEventListener('event type', callbackListener);
-button.addEventListener('click', btnAlert);
-
+// button.addEventListener('click', btnAlert);
 
 // 3. маємо щось зробити +
-function btnAlert() {
-  alert('Hello from JS');
-}
+// function btnAlert() {
+//   alert('Hello from JS');
+// }
 
 /*
   зробити лічільник
   по кліку на кнопку має алертом виводитись кількість кліків по кнопці
 */
+
+const taskBtn = document.getElementsByTagName('button')[0];
+
+// let clicks = 0;
+
+// function clickHandler () {
+//   alert(`Clicks: ${++clicks}`);
+// }
+
+// taskBtn.addEventListener('click',clickHandler);
+
+function createClickHandler() {
+  let clicks = 0;
+
+  return function clickHandler() {
+    alert(`Clicks: ${++clicks}`);
+  };
+}
+
+// const clickHandler = createClickHandler();
+
+// taskBtn.addEventListener('click',clickHandler);
+
+taskBtn.addEventListener('click', createClickHandler());
